545f6291455b8b63a129e27c27f40cd07b00a6ba
Ola Angelsmark <ola.angelsmark@ericsson.com>
2017-03-29 06:31:17 +0000

    stage3: install recent version of pip & calvin

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -1,9 +1,22 @@
 #!/bin/bash -e
 
+# fetch pip
+wget https://bootstrap.pypa.io/get-pip.py -O files/get-pip.py
+cp files/get-pip.py ${ROOTFS_DIR}/tmp
+
+# fetch calvin
+wget https://github.com/EricssonResearch/calvin-base/archive/master.zip -O files/calvin.zip
+
+install -v -o 1000 -g 1000 -d ${ROOTFS_DIR}/home/pi/Calvin
+unzip files/calvin.zip -d ${ROOTFS_DIR}/home/pi/Calvin
+chown 1000:1000 ${ROOTFS_DIR}/home/pi/Calvin -Rv
+
 on_chroot << EOF
-    cd /home/pi
-    git clone https://www.github.com/EricssonResearch/calvin-base Calvin
-    cd Calvin
-    sudo pip install -r requirements -r test-requirements -e .
+    cd /tmp
+    sudo python get-pip.py
+    cd /home/pi/Calvin/calvin-base-master
+    sudo pip install -r requirements.txt
+    sudo pip install -r test-requirements.txt
+    sudo pip install -e .
 EOF
 
