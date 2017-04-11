ed66f99cde2a7dc5b92e7be5bdba7801e5d37c6f
Robert Marklund <robert.marklund@ericsson.com>
2017-04-11 10:47:46 +0200

    add commit id of calvin and remove date from IMG_NAME
    Signed-off-by: Robert Marklund <robert.marklund@ericsson.com>

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -e
 
+CALVIN_GITREF=${CALVIN_GITREF:-master}
+
+CALVIN_BUNDLE="files/calvin.tgz"
+
 # fetch pip
 wget https://bootstrap.pypa.io/get-pip.py -O files/get-pip.py
-cp files/get-pip.py ${ROOTFS_DIR}/tmp
+mv files/get-pip.py ${ROOTFS_DIR}/tmp
 
-# fetch calvin
-wget https://github.com/EricssonResearch/calvin-base/archive/master.zip -O files/calvin.zip
+# Fetch calvin
+curl -L "https://api.github.com/repos/EricssonResearch/calvin-base/tarball/$CALVIN_GITREF" > $CALVIN_BUNDLE
 
 install -v -o 1000 -g 1000 -d ${ROOTFS_DIR}/home/pi/Calvin
-unzip files/calvin.zip -d ${ROOTFS_DIR}/home/pi/Calvin
+tar zxvf $CALVIN_BUNDLE -d ${ROOTFS_DIR}/home/pi/Calvin
 chown 1000:1000 ${ROOTFS_DIR}/home/pi/Calvin -Rv
 
 cp files/calvin.conf ${ROOTFS_DIR}/home/pi/Calvin
@@ -18,9 +22,12 @@ on_chroot << EOF
     cd /tmp
     sudo python get-pip.py
     sudo pip install -U six
-    cd /home/pi/Calvin/calvin-base-master
+    cd /home/pi/Calvin/EricssonResearch-calvin-base-*
     sudo pip install -r requirements.txt
     sudo pip install -r test-requirements.txt
     sudo pip install -e .
 EOF
 
+# Cleanup
+rm -fr ${ROOTFS_DIR}/home/pi/Calvin/EricssonResearch-calvin-base-*
+rm -f  ${ROOTFS_DIR}/tmp/get-pip.py
